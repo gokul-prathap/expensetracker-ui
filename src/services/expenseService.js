474c83c68829408dd1ccfb8e7f0563efea8c8e66
gokul-prathap <gokulprathap@rocketmail.com>
2024-08-28 21:06:41 +0530

    cors

--- src/services/expenseService.js
+++ src/services/expenseService.js
@@ -50,11 +50,14 @@ import axios from 'axios';
 
 const bearer_token = 'iSYgAYVt8924RZAs4zDdg48n';
 const bearer = `Bearer ${bearer_token}`;
-const API_URL = process.env.REACT_APP_API_URL || 'https://expensetracker-api-gokul-prathap-gokuls-projects-772978f1.vercel.app/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://cors-anywhere.herokuapp.com/https://expensetracker-api-gokul-prathap-gokuls-projects-772978f1.vercel.app/api';
 // const API_URL = 'http://localhost:8080/api';
 const headers = {
   Authorization: bearer,
-  'Access-Control-Allow-Origin': 'http://localhost:3000'
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type'
+
 }
 
 export const getAllExpenses = async () => {
